6c458c635f157eaa2935fc7b8b15a3d5eaeb4f17
Mr-Darkson <skybotsi@mail.ru>
2024-09-23 16:07:47 +0300

    fix name of major class

--- src/main/java/com/example/taskmanagment/TaskManagerApplication.java
+++ src/main/java/com/example/taskmanagment/TaskManagerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskMaganerApplication {
+public class TaskManagerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(TaskMaganerApplication.class, args);
+        SpringApplication.run(TaskManagerApplication.class, args);
     }
 
 }
--- src/test/java/com/example/taskmanagment/TaskManagerApplicationTests.java
+++ src/test/java/com/example/taskmanagment/TaskManagerApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TaskMaganerApplicationTests {
+class TaskManagerApplicationTests {
 
     @Test
     void contextLoads() {
